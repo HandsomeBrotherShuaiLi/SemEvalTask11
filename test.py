d735b8ee59c2bbbad9a7ff069ff38d21730c5e1d
shuai.li <shuai.li@shopee.com>
2020-02-24 12:25:46 +0800

    add word_level processing

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import numpy as np
-import os
+import os,json
 def test_for_words():
     p='V2/word_level/train_labels'
     le=[]
@@ -8,7 +8,15 @@ def test_for_words():
         le.append(len(f))
     print(le)
     print(max(le))
-
+def test_for_word_level_prediction():
+    json_path='results/dev_lstm_Word-level_lazyadam_Fixed-length-512_No-embedding_val_f1_2_1.json'
+    txt_path='results/dev_lstm_Word-level_lazyadam_Fixed-length-512_No-embedding_val_f1_2_1.txt'
+    prediction=json.load(open(json_path,'r',encoding='utf-8'))
+    for p in prediction:
+        word_index=np.load(p)
+        words=prediction[p]
+        for word in words:
+            print(p,word,word_index[word,:])
 
 if __name__=='__main__':
-    test_for_words()
+    test_for_word_level_prediction()
